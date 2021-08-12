a7ade8055d581a6b5ed0d690a05bcee22ef5eab3
Sanyat Hoque <sanyat.hoque@gmail.com>
2021-08-12 20:19:57 +0600

    Update authMiddlewareRoute.js

--- JWTAuth/backend/middleware/authMiddlewareRoute.js
+++ JWTAuth/backend/middleware/authMiddlewareRoute.js
@@ -2,22 +2,25 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 const authMiddlewareRoute= (req,res,next) => {
-    if (req.cookies.token) {console.log(`authMiddlewareRoute ==> Receive Cookie in the Server :  ${req.cookies.token}`.blue.underline)};
-    const token = req.cookies.token;
-    jwt.verify(token,'secretkey', async (err,decoded) => {
-        console.log('Successfully Decoded Message in authMiddlewareRoute ==>', decoded.id);
-        const {id} = decoded;
-        const user = await User.findOne({ '_id':decoded.id });
-        console.log('found user', user);
-        });
-    if (user) {
-        next();
-    } else {
+    try {
+        if (req.cookies.token) {
+            console.log(`authMiddlewareRoute ==> Receive Cookie in the Server :  ${req.cookies.token}`.blue.underline);
+            const token = req.cookies.token;
+            jwt.verify(token,'secretkey', async (err,decoded) => {
+                console.log('Successfully Decoded Message in authMiddlewareRoute ==>', decoded.id);
+                const {id} = decoded;
+                const user = await User.findOne({ '_id':decoded.id });
+                console.log('found user', user);
+                });
+                next();
+            };
+            // if (user) { next() }; 
+    } catch(err) {
         res.status(401);
-        res.redirect('/login');
-        throw new Error("Not authorized, Token Not Found");
+        throw new Error("Not authorized, User Not Verified",err);
         
     };
 };
 
 export { authMiddlewareRoute };
+
